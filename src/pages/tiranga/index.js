c06bf6848776617ed9b9c4610f324aeaa5a9c499
Dinesh Venkata Manikanta Maddirala <dinmad8851@students.ecpi.edu>
2023-08-13 12:38:22 -0400

    Created Pages Programmatically

--- src/pages/tiranga/index.js
+++ src/pages/tiranga/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
-import Layout from '../components/layout'
-import Seo from '../components/seo'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
 
 
 const TirangaPage = ({ data }) => {
@@ -10,9 +10,12 @@ const TirangaPage = ({ data }) => {
       {
         data.allMdx.nodes.map((node) => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+          <h2>
+              <Link to={`/tiranga/${node.frontmatter.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
-            <p>{node.excerpt}</p>
           </article>
         ))
       }
@@ -27,9 +30,9 @@ export const query = graphql`
         frontmatter {
           date(formatString: "MMMM D, YYYY")
           title
+          slug
         }
         id
-        excerpt
       }
     }
   }
