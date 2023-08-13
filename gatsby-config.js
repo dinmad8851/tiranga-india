2a8017366056b6e1e42da9530e85d2a335a66e56
Dinesh Venkata Manikanta Maddirala <dinmad8851@students.ecpi.edu>
2023-08-13 11:09:56 -0400

    Transforming Data to Use MDX

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,6 @@ module.exports = {
         path: `${__dirname}/tiranga`,
       }
     },
+    "gatsby-plugin-mdx",
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,21 @@ import Seo from '../components/seo'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>This site provide you with the significance of the Tiranga Colors in the Indian National Flag</p>
+      <h2>Introduction</h2>
+        <p>The Indian National Flag, also known as the "Tiranga," holds immense significance and symbolism
+        for the people of India. Comprising three horizontal stripes of saffron, white, and green, with
+        the Ashoka Chakra in the center, the flag represents the unity, diversity, and aspirations of 
+        a nation that has a rich history and a promising future. The colors of the Tiranga are not just 
+        visual elements; they embody the essence of India's struggle for independence, its cultural
+        diversity, and its enduring commitment to peace and progress.</p>
+      
+      <h3>Ashoka Chakra: The Wheel of Progress</h3>
+        <p>The Ashoka Chakra, a navy blue wheel with 24 spokes, represents the dynamic progress and constant
+         movement of the nation. Inspired by the Lion Capital of Ashoka, the Ashoka Chakra embodies values 
+         such as righteousness, justice, and dharma. It serves as a reminder of India's rich heritage and 
+         its commitment to ethical governance and social harmony.</p>
       <StaticImage
-        alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+        alt="Tiranga, with the Ashoka Chakra"
         src="../images/indian-flag-short.jpg"
       />
     </Layout>
